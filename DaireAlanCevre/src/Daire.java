55b67afd5ba2cab0cfbd427c73a3be52c6038442
GÖKALP <gokalppabdullah@gmail.com>
2023-06-19 22:44:14 +0300

    Add files via upload

---
+++ DaireAlanCevre/src/Daire.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+
+public class Daire {
+    public static void main(String[] args) {
+        int r;
+        double pi = 3.14;
+
+        Scanner inp = new Scanner(System.in);
+
+        System.out.println("Dairenin Yarı Çapını Giriniz: ");
+        r = inp.nextInt();
+
+        double alan = pi * r * r;
+        double cevre = 2 * pi * r;
+
+
+        System.out.println("Dairenin Alanı: " +alan);
+        System.out.println("Dairenin Çevresi: " +cevre);
+
+
+
+    }
+}
